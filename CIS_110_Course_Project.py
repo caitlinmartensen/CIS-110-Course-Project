c5e8a53d7426accb6d7b11836743e390f1a75f9a
username <useremail>
2024-09-09 10:29:30 -0700

    Data valadation loops

--- CIS_110_Course_Project.py
+++ CIS_110_Course_Project.py
@@ -33,39 +33,49 @@ print(f"While looking for {favoriteFood} he saw a farm house!")
 print(f"This {favoriteColor} house he had not seen before!")
 
 goTofarmHouse = input (f"Should {mouseName} go to the farm house? Type yes or no:  ")
+while goTofarmHouse.lower() != "yes" and goTofarmHouse.lower() != "no":
+    goTofarmHouse = input(f"Please type yes or no:  ")
+    
 if goTofarmHouse == "yes":
+    print(f"...")
     print(f"{mouseName} heads to the farm house and sees a family and their dog outside on a {season} day.")
     print(f"The family's dog spots {mouseName} and runs after him, chasing him away!")
     print(f"{mouseName} runs as fast as he can to get away.")
     print(f"No luck in getting any {favoriteFood} from there today!")
 else:
+    print(f"...")
     print(f"{mouseName} sits on the edge of the cornfiedl and watches the family and their dog.")
     print(f"He watches them enjoy the {season} day!")
     print(f"After watching a while he decides it's time to move on and keep looking for some food.")
     
-goTospookyHouse = input(f"n\{mouseName} continues to walk along and sees an abandon SPOOKY house. Should {mouseName} go to the spooky house? Type Yes or no:  ")
+goTospookyHouse = input(f"{mouseName} continues to walk along and sees an abandon SPOOKY house. Should {mouseName} go to the spooky house? Type Yes or no:  ")
 while goTospookyHouse.lower() != "yes" and goTospookyHouse.lower != "no":
     goTospookyHouse = input(f"Please type yes or no:  ")
     
 if goTospookyHouse == "yes":
+    print(f"...")
     print(f"Hoping to find some {favoriteFood} at this place, {mouseName} heads on over to the SPOOKY house.")
     print(f"{mouseName} Enters the house and scurries to the kitchen to see what can be scavenged.")
     print(f"Upon entering the kitchen {mouseName} sees a GHOST floating in front of the fridge!!")
     print(f"Scared out of his mind, {mouseName} runs out of the house!!")
 else:
+    print(f"...")
     print(f"Too aftaid of the SPOOKY house, {mouseName} heads to the back to seee if any food can be found.")
     print(f"Around the back of the house the overgrown garden looks like a jungle.")
     print(f"Too afraid of the darkness, from what looks like a forbidden forrest, {mouseName} leaves.")
     
 if goTofarmHouse == "yes" and goTospookyHouse == "yes":
+    print(f"...")
     print(f"After and exhusting day of running, our mouse heads home.")
     print(f"{mouseName} gets home to find a nice meal on the table from his wife!")
 elif goToFarmHouse == "no" and goTospookyHouse == "no":
+    print("...")
     print(f"Defeated with an empty belly, {mouseName} heads home.")
     print(f"On his way ome he finds a Happy Mean on the side of the road!!")
     print(f"SCORE ONE FOR THE LITTLE GUY!!")
     print(f"{mouseName} feels great bringing home some chicky nugs.")
 else:
+    print(f"...")
     print(f"After an adventurous day, {mouseName} heads home.")
     print(f"On his way he sees a {favoriteColor} {favoriteFlower} and grabs it for his wife.") 
     print(f"He remembers how lucky he is to have her.")
